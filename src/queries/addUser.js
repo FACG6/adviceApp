4d985cb57a149772dbaac8b859f86045e9e4a56e
Khader <kakastarcris@gmail.com>
2019-02-19 15:58:19 +0200

    arrange code

--- src/queries/addUser.js
+++ src/queries/addUser.js
@@ -1,10 +1,10 @@
 const connection = require('../database/db_connection');
 
-exports.insertUser = (firstName, lastName, username, email, password) => {
+exports.insertUser = (firstName, lastName, email, password) => {
     return new Promise((resolve, reject) => {
-      connection.query('INSERT INTO users (first_name, last_name, username, email, password) VALUES ($1, $2, $3, $4, $5) returning *', [firstName, lastName, username, email, password], (err, res) => {
+      connection.query('INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5) returning *', [firstName, lastName, email, password], (err, res) => {
       if (err) {
-        reject(err);              
+        reject(err);
       } else {
         resolve(res);
       }          
