e000bf6955a3f4a47acfd7184b427adeacd698f3
shorouq <shrooqabdullahsaad@gmail.com>
2019-02-19 15:45:44 +0200

    modify indentation

--- src/database/db_built.js
+++ src/database/db_built.js
@@ -5,13 +5,11 @@ const dbConnection = require('./db_connection');
 const sqlFilePath = path.join(__dirname, 'db_built.sql');
 const sql = fs.readFileSync(sqlFilePath).toString();
 
-const runDbBuild = () => {
-    return new Promise((reslove, reject) => {
-        dbConnection.query(sql, (err, res)=>{
-            if (err) reject(err);
-            else reslove(res);
-        })
-    });
-}
+const runDbBuild = () => new Promise((reslove, reject) => {
+  dbConnection.query(sql, (err, res) => {
+    if (err) reject(err);
+    else reslove(res);
+  });
+});
 
 module.exports = runDbBuild;
