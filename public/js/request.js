572eef2ce7b52903be8d59cae563064571586627
shorouq <shrooqabdullahsaad@gmail.com>
2019-02-19 15:45:09 +0200

    make promise for request

--- public/js/request.js
+++ public/js/request.js
@@ -1,21 +1,23 @@
 const request = (url, method, value, cb) => {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-      const response = JSON.parse(xhr.responseText);
-      if (xhr.status === 200) {
-        if (response.error) {
-          cb(new TypeError(response.error));
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        const response = JSON.parse(xhr.responseText);
+        if (xhr.status === 200) {
+          if (response.error) {
+            reject(response.error);
+          } else {
+            cb(null, JSON.parse(response.results));
+          }
         } else {
-          cb(null, JSON.parse(response.results));
+          cb(new TypeError(response.error));
         }
-      } else {
-        cb(new TypeError(response.error));
       }
-    }
-  };
-  xhr.open(method, url);
-  xhr.send(value);
+    };
+    xhr.open(method, url);
+    xhr.send(value);
+  });
 };
 if (typeof module !== 'undefined') {
   module.exports = request;
