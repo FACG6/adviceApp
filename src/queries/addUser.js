a1085936d967bbd909ab4c92a4702e4bc982775d
Khader <kakastarcris@gmail.com>
2019-02-19 14:36:57 +0200

    modify req. is done

--- src/queries/addUser.js
+++ src/queries/addUser.js
@@ -2,15 +2,17 @@ const connection = require('../');
 
 exports.insertUser = (firstName, lastName, username, email, password, callback) => {
   let sql = {
-    text: 'INSERT INTO users (fName, lName, username, email, pass) VALUES ($1, $2, $3, $4, $5)',
+    text: 'INSERT INTO users (fName, lName, username, email, pass) VALUES ($1, $2, $3, $4, $5) returning *',
     VALUES = [firstName, lastName, password, email, password]
   };
 
   connection.query(sql, (err, res) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, res.rows);
-    }
+    return new Promise((resolve, reject) => {
+      if (err) {
+        reject(err);              
+      } else {
+        resolve(res);
+      }          
+    });
   });
 }
